fa6212c8fa202c0aca661569c297b629d217c8ec
MCNatsu <howardwu83@gmail.com>
2022-11-30 20:25:40 +0800

    初始架構

---
+++ website/flask/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+def create_app():
+    app = Flask(__name__)
+
+    from .views import views
+
+    app.register_blueprint(views,url_prefix='/')
+    app.register_error_handler(404, page_not_found)
