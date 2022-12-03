34a6f7cf1ff8b767861768bb097fd6707cdc8b43
MCNatsu <howardwu83@gmail.com>
2022-12-03 15:46:13 +0800

    新增加載畫面

--- website/static/JavaScript/loading.js
+++ website/static/JavaScript/loading.js
@@ -1,11 +1,6 @@
-(function($){
-    $(window).load(function(){
-        $('#begin').fadeOut();
-        $('#loading').delay(200).fadeOut('slow');
-    });
- 
-	$(document).ready(function(){
-	//code
-	})
- 
-})(jQuery);
+(function($) {
+    $( window ).on("load", function (e) {
+        $("#status").delay(2000).fadeOut(2000); //delay --> 延遲幾秒才fadeOut
+        $("#preloader").delay(4000).fadeOut(1000);
+      })
+})(jQuery)
