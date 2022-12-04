ab8d50b066d7da041da6a52bb32e3d6859834883
MCNatsu <howardwu83@gmail.com>
2022-12-04 15:54:08 +0800

    Merge with Sam's Code

--- website/views.py
+++ website/views.py
@@ -1,8 +1,19 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,abort,url_for,redirect
+import os
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
+    return redirect(url_for('views.getpage', page="home"))
 
-    return render_template("home.html")
+
+@views.route('/page/<page>')
+def getpage(page):
+    if os.path.isfile(f"website/Markdown/{page}.md"):
+        with open(f"website/Markdown/{page}.md",encoding="UTF-8") as f:
+            lines = f.read()
+
+        return render_template("home.html",post=lines)
+
+    return abort(404)
