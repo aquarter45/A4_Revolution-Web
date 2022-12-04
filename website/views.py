5aca6a49a083834e58c23e08db8301430fb411a5
MCNatsu <howardwu83@gmail.com>
2022-12-04 16:37:05 +0800

    redirect md files

--- website/views.py
+++ website/views.py
@@ -8,12 +8,11 @@ def home():
     return redirect(url_for('views.getpage', page="home"))
 
 
-@views.route('/page/<page>')
+@views.route('/<page>')
 def getpage(page):
-    if os.path.isfile(f"website/Markdown/{page}.md"):
-        with open(f"website/Markdown/{page}.md",encoding="UTF-8") as f:
+    if os.path.isfile(f"website/Markdown/{page}/{page}.md"):
+        with open(f"website/Markdown/{page}/{page}.md",encoding="UTF-8") as f:
             lines = f.read()
-
-        return render_template("home.html",post=lines)
+        return render_template("home.html",annoucement=lines)
 
     return abort(404)
