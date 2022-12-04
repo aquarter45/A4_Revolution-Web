7d5a340bea728c6ba028e4a5a5ed6f4f75989f3b
MCNatsu <howardwu83@gmail.com>
2022-12-04 16:36:46 +0800

    markdown render added

--- website/about_us.py
+++ website/about_us.py
@@ -1,7 +1,17 @@
-from flask import Blueprint , render_template
+from flask import Blueprint,render_template,abort,url_for,redirect
+import os
 
 about_us = Blueprint('about_us', __name__)
 
 @about_us.route('/')
 def abouts_us():
-    return render_template("/about-us/about-us.html")
+    return redirect(url_for('about_us.getPage', page="our_goals"))
+
+@about_us.route('/<page>')
+def getPage(page):
+    if os.path.isfile(f"website/Markdown/{page}/{page}.md"):
+        with open(f"website/Markdown/{page}/{page}.md",encoding="UTF-8") as f:
+            lines = f.read()
+        return render_template("about-us/our_goals.html",our_goals=lines)
+
+    return abort(404)
--- website/support_us.py
+++ website/support_us.py
@@ -1,8 +1,18 @@
-from flask import Blueprint , render_template
-from flask import jsonify
+from flask import Blueprint,render_template,abort,url_for,redirect
+import os
 
 support_us = Blueprint('support_us', __name__)
 
 @support_us.route('/')
 def supportus_Home():
-    return render_template("/support-us/support-us.html")
+    return redirect(url_for("support_us.getpage", page='support-us'))
+
+@support_us.route('/<page>')
+def getpage(page):
+    if os.path.isfile(f"website/Markdown/{page}/{page}.md"):
+        with open(f"website/Markdown/{page}/{page}.md",encoding="UTF-8") as f:
+            lines = f.read()
+        return render_template("support-us/support-us.html",support_us=lines)
+
+    return abort(404)
+    
